aec9f115ef93e0218dcecd25298a73ea013330a7
pranav-kalra <42677927+pranav-kalra@users.noreply.github.com>
2018-09-09 13:37:43 -0700

    First working version

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.printf(" l(a %n %n %n le %n %n af %n %n fa %n %n %n ll %n %n %n s) %n %n one %n %n l %n %n %n iness");
+
+    }
+}
