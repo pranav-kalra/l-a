1db2568364859bdf512ab35caaab5a0eddf43187
pranav-kalra <42677927+pranav-kalra@users.noreply.github.com>
2018-09-13 11:56:32 -0700

    Fixed the line break

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.printf(" l(a %n %n %n le %n %n af %n %n fa %n %n %n ll %n %n %n s) %n %n one %n %n l %n %n %n iness");
+        System.out.printf(" l(a %n %n le %n af %n fa %n %n ll %n %n s) %n one %n l %n %n iness");
 
     }
 }
